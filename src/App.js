bdb84750218d50c3585b0e09bd83154be1c36100
Devanshu Kumar <devanshukumar289@gmail.com>
2024-02-01 16:53:10 +0530

    Header Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import i18n from "./i18n";
 import { I18nextProvider } from "react-i18next";
 
