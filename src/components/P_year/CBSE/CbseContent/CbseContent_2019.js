ce057b5d6a6a9a879bb4299dee84036140f2ec31
devansh98350 <devanshkumar289@gmail.com>
2025-05-11 14:04:46 +0530

    Refactor CBSE content

--- src/components/P_year/CBSE/CbseContent/CbseContent_2019.js
+++ src/components/P_year/CBSE/CbseContent/CbseContent_2019.js
@@ -1,55 +1,220 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 import Layout from "../../../Layout";
-import "./CbseContent.css";
-import physics from "../../../../assests/Pages/physics.png";
-import chemistry from "../../../../assests/Pages/chemistry.png";
-import maths from "../../../../assests/Pages/maths.png";
-import biology1 from "../../../../assests/Pages/biology1.png";
-import english from "../../../../assests/Pages/english.png";
-import painting from "../../../../assests/Pages/painting.png";
 import Breadcrumbs from "../../../Breadcrumbs";
+import Explore from "../../../Common/Explore";
+import Banner from "../../../Common/Banner";
+import img7 from "../../../../assests/home_page/dev1.png";
+import img8 from "../../../../assests/home_page/dev2.png";
+import img9 from "../../../../assests/home_page/dev3.png";
+import img10 from "../../../../assests/home_page/dev4.png";
 
-function CbseContent2019() {
+const CbsePYQ2019 = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+  const questionsData12 = [
+    {
+      id: "",
+      title: "Physics",
+      shift: "CBSE 2019 (Class-12)",
+      bgColor: "bg-[#5dc298]",
+      image: img7,
+    },
+    {
+      id: "",
+      title: "Chemistry",
+      shift: "CBSE 2019 (Class-12)",
+      bgColor: "bg-[#5dc298]",
+      image: img8,
+    },
+    {
+      id: "",
+      title: "Biology",
+      shift: "CBSE 2019 (Class-12)",
+      bgColor: "bg-[#5dc298]",
+      image: img9,
+    },
+    {
+      id: "",
+      title: "Mathematics",
+      shift: "CBSE 2019 (Class-12)",
+      bgColor: "bg-[#5dc298]",
+      image: img10,
+    },
+    {
+      id: "",
+      title: "English",
+      shift: "CBSE 2019 (Class-12)",
+      bgColor: "bg-[#5dc298]",
+      image: img7,
+    },
+  ];
+  const questionsData10 = [
+    {
+      id: "",
+      title: "Science",
+      shift: "CBSE 2019 (Class-10)",
+      bgColor: "bg-[#5dc298]",
+      image: img9,
+    },
+    {
+      id: "",
+      title: "Mathematics",
+      shift: "CBSE 2019 (Class-10)",
+      bgColor: "bg-[#5dc298]",
+      image: img10,
+    },
+    {
+      id: "",
+      title: "Social Studies",
+      shift: "CBSE 2019 (Class-10)",
+      bgColor: "bg-[#5dc298]",
+      image: img7,
+    },
+    {
+      id: "",
+      title: "English",
+      shift: "CBSE 2019 (Class-10)",
+      bgColor: "bg-[#5dc298]",
+      image: img7,
+    },
+    {
+      id: "",
+      title: "Sanskrit",
+      shift: "CBSE 2019 (Class-10)",
+      bgColor: "bg-[#5dc298]",
+      image: img8,
+    },
+  ];
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+  const itemVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.3,
+      },
+    },
+  };
   return (
-    <Layout title="CBSE-PYQ-2019-IIT-Academy">
+    <Layout title="CBSE 2019 PYQ - IIT Academy" keyword="" description="">
       <Breadcrumbs />
-      <div className="jeeContent-container">
-        <div>
-          <button className="jeeContent-card">
-            <img src={physics} alt="physics" />
-          </button>
+      <div className="container mx-auto px-4">
+        <div className="mb-10">
+          <h1 className="text-[2rem] font-bold text-blue ">
+            CBSE 2019 Question Paper
+          </h1>
         </div>
-        <div>
-          <button className="jeeContent-card">
-            <img src={chemistry} alt="chemistry" />
-          </button>
-        </div>
-        <div>
-          <button className="jeeContent-card">
-            <img src={maths} alt="maths" />
-          </button>
-        </div>
-        <div>
-          <button className="jeeContent-card">
-            <img src={biology1} alt="biology1" />
-          </button>
-        </div>
-        <div>
-          <button className="jeeContent-card">
-            <img src={english} alt="english" />
-          </button>
-        </div>
-        <div>
-          <button className="jeeContent-card">
-            <img src={painting} alt="painting" />
-          </button>
-        </div>
+        <h4 className="mt-[-2rem]">
+          <b>Class-12</b>
+        </h4>
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          {questionsData12.map((item) => (
+            <motion.div
+              whileHover={{
+                scale: 1.03,
+                boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.1)",
+              }}
+              key={item.id}
+              variants={itemVariants}
+              className="h-64 relative rounded-xl overflow-hidden shadow-md transition-transform hover:shadow-lg"
+            >
+              <Link to={`#`}>
+                <div
+                  className={`absolute inset-0 ${
+                    item.bgColor || "bg-blue-500"
+                  } bg-opacity-80`}
+                >
+                  <img
+                    src={item.image}
+                    alt=""
+                    className="w-full h-full object-cover opacity-60 "
+                  />
+                </div>
+                <div className="absolute inset-0 p-6 flex flex-col justify-between">
+                  <h2 className="text-white text-2xl font-bold">
+                    {item.title}
+                  </h2>
+                  <div className="flex items-center justify-between">
+                    <span className="text-white text-sm">{item.shift}</span>
+                    <span className="px-3 py-1 bg-white bg-opacity-20 text-white text-sm rounded-full">
+                      Click Here
+                    </span>
+                  </div>
+                </div>
+              </Link>
+            </motion.div>
+          ))}
+        </motion.div>
+        <h4>
+          <b>Class-10</b>
+        </h4>
+        <motion.div
+          className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          {questionsData10.map((item) => (
+            <motion.div
+              whileHover={{
+                scale: 1.03,
+                boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.1)",
+              }}
+              key={item.id}
+              variants={itemVariants}
+              className="h-64 relative rounded-xl overflow-hidden shadow-md transition-transform hover:shadow-lg"
+            >
+              <Link to={`#`}>
+                <div
+                  className={`absolute inset-0 ${
+                    item.bgColor || "bg-blue-500"
+                  } bg-opacity-80`}
+                >
+                  <img
+                    src={item.image}
+                    alt=""
+                    className="w-full h-full object-cover opacity-60 "
+                  />
+                </div>
+                <div className="absolute inset-0 p-6 flex flex-col justify-between">
+                  <h2 className="text-white text-2xl font-bold">
+                    {item.title}
+                  </h2>
+                  <div className="flex items-center justify-between">
+                    <span className="text-white text-sm">{item.shift}</span>
+                    <span className="px-3 py-1 bg-white bg-opacity-20 text-white text-sm rounded-full">
+                      Click Here
+                    </span>
+                  </div>
+                </div>
+              </Link>
+            </motion.div>
+          ))}
+        </motion.div>
       </div>
+      <div className="mt-6">
+        <Banner />
+      </div>
+      <Explore />
     </Layout>
   );
-}
+};
 
-export default CbseContent2019;
+export default CbsePYQ2019;
